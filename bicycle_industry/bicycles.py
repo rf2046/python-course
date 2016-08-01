ee5b94663edcd521363876276dac7861cd6658f2
rf2046 <ryotafunakoshi@gmail.com>
2016-08-01 16:27:54 -0400

    bicycle industry files

---
+++ bicycle_industry/bicycles.py
@@ -0,0 +1,17 @@
+class Bicycle:
+	
+	def __init__(self, model_name, weight, cost):
+		
+		self.model_name = model_name
+		self.weight = weight
+		self.cost = cost
+
+class Bike_Shops:
+	
+	def __init__(self, name, margin, bikes):
+
+		self.name = name
+		self.margin = margin
+		self.bikes = bikes
+
+class 
